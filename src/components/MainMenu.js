8181931fccec822d6cf9aa278b1868b0b1750eb8
a.elizarev <a.elizarev@havasfood.uz>
2024-06-13 13:44:48 +0500

    test

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import { Button, Container, Typography } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { Button, Container, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const MainMenu = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <Container>
-            <Typography variant="h4" gutterBottom>
-                Главное меню
-            </Typography>
-            <Button variant="contained" color="primary" onClick={() => navigate('/shop')}>
-                Работа с магазином
-            </Button>
-            <Button variant="contained" color="primary" style={{ marginLeft: '10px' }}>
-                Работа с ф/м
-            </Button>
-            <Button variant="contained" color="primary" style={{ marginLeft: '10px' }}>
-                Работа с заявками
-            </Button>
-        </Container>
-    );
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 1,
+      }}
+    >
+      <Typography variant="h4" gutterBottom>
+        Главное меню
+      </Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => navigate("/shop")}
+      >
+        Работа с магазином
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        style={{ marginLeft: "10px" }}
+      >
+        Работа с ф/м
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        style={{ marginLeft: "10px" }}
+      >
+        Работа с заявками
+      </Button>
+    </Container>
+  );
 };
 
 export default MainMenu;
