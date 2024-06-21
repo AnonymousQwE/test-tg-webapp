e226b69c348c940696e2c41f39d158d300edc58d
a.elizarev <a.elizarev@havasfood.uz>
2024-06-21 18:14:58 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainMenu from "./components/MainMenu";
 import Shop from "./components/Shop";
+import { Button, Container } from "@mui/material";
+import Header from "./components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
+  const onClose = () => {
+    tg.close();
+  };
+
+  const tg = window.Telegram.WebApp;
+
+  // tg.showAlert(`Добро пожаловать, ${tg?.WebAppUser?.username}`);
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainMenu />} />
-        <Route path="/shop" element={<Shop />} />
-      </Routes>
-    </Router>
+    <>
+      <Container style={{ marginTop: "20px" }}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<MainMenu />} />
+            <Route path="/shop" element={<Shop />} />
+          </Routes>
+        </BrowserRouter>
+        <Button onClick={onClose}>Закрыть</Button>
+        <span className={"username"}>{tg.initDataUnsafe?.user?.username}</span>
+      </Container>
+    </>
   );
 };
 
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import {
   Button,
-  Container,
   TextField,
   Typography,
   Grid,
   Card,
   CardContent,
+  CardHeader,
+  CircularProgress,
+  Box,
 } from "@mui/material";
 import {
   Search as SearchIcon,
@@ -15,67 +17,123 @@ import {
   Computer as ComputerIcon,
   PointOfSale as PointOfSaleIcon,
   Router as RouterIcon,
+  Scale as ScaleIcon,
+  Store as StoreIcon,
+  Wifi as WifiIcon,
 } from "@mui/icons-material";
+import axios from "axios";
 
 const Shop = () => {
   const [shopNumber, setShopNumber] = useState("");
   const [status, setStatus] = useState(null);
   const [lastUpdate, setLastUpdate] = useState(null);
-  const [equipmentStatuses, setEquipmentStatuses] = useState({});
+  const [equipmentStatuses, setEquipmentStatuses] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentTime, setCurrentTime] = useState(
+    new Date().toLocaleTimeString()
+  );
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date().toLocaleTimeString());
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const fetchPingResults = useCallback(async () => {
+    if (shopNumber) {
+      setEquipmentStatuses([]);
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          `https://webapp.havasfood.uz:11443/backend/ping/${shopNumber}`,
+          {
+            referrerPolicy: "unsafe-url",
+          }
+        );
+        const data = response.data;
+        console.log(data);
+        setEquipmentStatuses(data);
+        setLastUpdate(new Date().toLocaleTimeString());
+      } catch (error) {
+        console.error("Error fetching ping results:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+  }, [shopNumber]);
+
+  useEffect(() => {
+    if (shopNumber) {
+      const intervalId = setInterval(fetchPingResults, 30000);
+      return () => clearInterval(intervalId);
+    }
+  }, [shopNumber, fetchPingResults]);
 
   const handleSearch = () => {
-    // Заглушка для поиска магазина
-    if (shopNumber) {
-      setStatus(`Магазин ${shopNumber} найден`);
-      setEquipmentStatuses({
-        cashRegister1: "Работает",
-        cashRegister2: "Не работает",
-        pc: "Работает",
-        mikrotik: "Работает",
-      });
-      setLastUpdate(new Date().toLocaleTimeString());
-    } else {
-      setStatus("Магазин не найден");
-      setEquipmentStatuses({});
+    setStatus(`Магазин ${shopNumber}`);
+    setEquipmentStatuses([]);
+    fetchPingResults();
+  };
+
+  const handleShopNumberChange = (e) => {
+    setShopNumber(e.target.value);
+    setStatus(null);
+    setEquipmentStatuses([]);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
     }
   };
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (shopNumber) {
-        // Обновляем статус оборудования (заглушка)
-        setStatus(`Магазин ${shopNumber} найден`);
-        setEquipmentStatuses({
-          cashRegister1: "Работает",
-          cashRegister2: "Не работает",
-          pc: "Работает",
-          mikrotik: "Работает",
-        });
-        setLastUpdate(new Date().toLocaleTimeString());
-      }
-    }, 10000);
-
-    return () => clearInterval(interval);
-  }, [shopNumber]);
-
-  const renderStatusIcon = (status) => {
-    return status === "Работает" ? (
+  const renderStatusIcon = (alive) => {
+    return alive ? (
       <CheckCircleIcon color="success" />
     ) : (
       <CancelIcon color="error" />
     );
   };
 
+  const renderHostName = (host) => {
+    const ip = host.split(".");
+    const last = ip[ip.length - 1];
+    switch (last) {
+      case "254":
+        return ["Mikrotik", <RouterIcon fontSize="large" key="mikrotik" />];
+      case "10":
+        return ["Компьютер", <ComputerIcon fontSize="large" key="computer" />];
+      case "160":
+        return ["Unify", <WifiIcon fontSize="large" key="unify" />];
+      case "60":
+        return ["Весы", <ScaleIcon fontSize="large" key="scale" />];
+      case "101":
+        return ["Касса 1", <PointOfSaleIcon fontSize="large" key="pos1" />];
+      case "102":
+        return ["Касса 2", <PointOfSaleIcon fontSize="large" key="pos2" />];
+      case "103":
+        return ["Касса 3", <PointOfSaleIcon fontSize="large" key="pos3" />];
+      default:
+        return [
+          "Неизвестное устройство",
+          <StoreIcon fontSize="large" key="unknown" />,
+        ];
+    }
+  };
+
   return (
-    <Container>
-      <Typography variant="h4" gutterBottom>
+    <>
+      <Typography variant="h4" gutterBottom align="center">
         Работа с магазином
       </Typography>
+
       <TextField
         label="Номер магазина"
         variant="outlined"
         value={shopNumber}
-        onChange={(e) => setShopNumber(e.target.value)}
+        onChange={handleShopNumberChange}
+        onKeyPress={handleKeyPress}
         fullWidth
         margin="normal"
       />
@@ -85,72 +143,59 @@ const Shop = () => {
         onClick={handleSearch}
         style={{ marginTop: "10px" }}
         startIcon={<SearchIcon />}
+        fullWidth
       >
         Искать магазин
       </Button>
-      {status && (
-        <>
-          <Typography variant="h6" gutterBottom style={{ marginTop: "20px" }}>
-            {status}
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            Последнее обновление: {lastUpdate}
-          </Typography>
-          {Object.keys(equipmentStatuses).length > 0 && (
-            <Grid container spacing={2} style={{ marginTop: "10px" }}>
-              <Grid item xs={12} sm={6} md={3}>
-                <Card>
-                  <CardContent>
-                    <Typography variant="h6">Касса 1</Typography>
-                    <PointOfSaleIcon fontSize="large" />
-                    <Typography variant="body1">
-                      {renderStatusIcon(equipmentStatuses.cashRegister1)}{" "}
-                      {equipmentStatuses.cashRegister1}
-                    </Typography>
-                  </CardContent>
-                </Card>
+      {loading ? (
+        <Box display="flex" justifyContent="center" alignItems="center" mt={4}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        status && (
+          <>
+            <Typography
+              variant="h6"
+              gutterBottom
+              style={{ marginTop: "20px" }}
+              align="center"
+            >
+              {status}
+            </Typography>
+            <Typography variant="body1" gutterBottom align="center">
+              Последнее обновление: {lastUpdate}
+            </Typography>
+            <Typography variant="body1" align="center">
+              Текущее время: {currentTime}
+            </Typography>
+            {equipmentStatuses.length > 0 && (
+              <Grid container spacing={2} style={{ marginTop: "10px" }}>
+                {equipmentStatuses.map((h) => (
+                  <Grid item xs={12} sm={6} md={4} lg={3} key={h.host}>
+                    <Card>
+                      <CardHeader
+                        avatar={renderHostName(h.host)[1]}
+                        title={renderHostName(h.host)[0]}
+                        titleTypographyProps={{ variant: "h6" }}
+                      />
+                      <CardContent>
+                        <Typography
+                          variant="body2"
+                          color="textSecondary"
+                          component="p"
+                        >
+                          {renderStatusIcon(h.alive)} Max: {h.max} Min: {h.min}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                ))}
               </Grid>
-              <Grid item xs={12} sm={6} md={3}>
-                <Card>
-                  <CardContent>
-                    <Typography variant="h6">Касса 2</Typography>
-                    <PointOfSaleIcon fontSize="large" />
-                    <Typography variant="body1">
-                      {renderStatusIcon(equipmentStatuses.cashRegister2)}{" "}
-                      {equipmentStatuses.cashRegister2}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} sm={6} md={3}>
-                <Card>
-                  <CardContent>
-                    <Typography variant="h6">ПК</Typography>
-                    <ComputerIcon fontSize="large" />
-                    <Typography variant="body1">
-                      {renderStatusIcon(equipmentStatuses.pc)}{" "}
-                      {equipmentStatuses.pc}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} sm={6} md={3}>
-                <Card>
-                  <CardContent>
-                    <Typography variant="h6">Микротик</Typography>
-                    <RouterIcon fontSize="large" />
-                    <Typography variant="body1">
-                      {renderStatusIcon(equipmentStatuses.mikrotik)}{" "}
-                      {equipmentStatuses.mikrotik}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            </Grid>
-          )}
-        </>
+            )}
+          </>
+        )
       )}
-    </Container>
+    </>
   );
 };
 
